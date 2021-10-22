bffa6ed256e1ffda5ef82bdf4c75cec5f09a29d0
subrat jena <subratj611@gmail.com>
2021-10-22 14:34:31 +0530

    First Commit

---
+++ main.js
@@ -0,0 +1,35 @@
+let hamburger = document.querySelector('.fas.fa-bars');
+let sidebar = document.querySelector('.sidebar');
+
+let login = document.querySelector('.auth_login');
+let loginForm = document.querySelector('.auth_login');
+
+
+// buttons
+let lgLoginBtn = document.querySelector('.lg-screen.login-btn');
+let mobLoginBtn = document.querySelector('.mob.login-btn');
+
+let cancelIcon = document.querySelector('.fas.login_form');
+
+
+// Mobile Bar
+
+hamburger.addEventListener('click', ()=>{
+    sidebar.classList.toggle('show');
+});
+
+
+//  Login Form
+
+lgLoginBtn.addEventListener('click',()=>{
+    loginForm.classList.add('show');
+})
+
+mobLoginBtn.addEventListener('click', ()=>{
+    loginForm.classList.add('show');
+    sidebar.classList.remove('show');
+})
+
+cancelIcon.addEventListener('click', ()=>{
+    loginForm.classList.remove('show');    
+})
